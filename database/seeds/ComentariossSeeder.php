9893c1e9f9cb29200fa948c8f08e96c36c67e36d
RUBENXD713 <71050267+RUBENXD713@users.noreply.github.com>
2020-10-20 23:10:33 -0500

    Seeders

--- database/seeds/ComentariossSeeder.php
+++ database/seeds/ComentariossSeeder.php
@@ -11,6 +11,23 @@ class ComentariossSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('comentarios')->insert([
+            'Contenido' => "Que increible!!",
+            'productos' => 1,
+            'Contenido' => "Lo mejor que eh visto!!",
+            'productos' => 6,
+            'Contenido' => "No manches queeee!!",
+            'productos' => 2,
+            'Contenido' => "Te puedo dar mi opinion!!",
+            'productos' => 3,
+            'Contenido' => "Calidad Precio!!",
+            'productos' => 7,
+            'Contenido' => "What!!",
+            'productos' => 5,
+            'Contenido' => "Esto es lo peor que puedes comprar!!",
+            'productos' => 4,
+            
+        ]);
         $comentarios=Factory(App\Comentarioss::class,15)->create();
     }
 }
--- database/seeds/ProductosSeeder.php
+++ database/seeds/ProductosSeeder.php
@@ -12,7 +12,14 @@ class ProductosSeeder extends Seeder
     public function run()
     {
         DB::table('productos')->insert([
-            
+            'Nombre'=> 'Mac',
+            'Nombre'=> 'Skate futurista',
+            'Nombre'=> 'Carro',
+            'Nombre'=> 'Pc Gamer',
+            'Nombre'=> 'Cellphone',
+            'Nombre'=> 'Iphone',
+            'Nombre'=> 'Huawei',
+            'Nombre'=> 'MinePoke',
         ]);
         $productos=factory(App\Productos::class,5)->create();
     }
